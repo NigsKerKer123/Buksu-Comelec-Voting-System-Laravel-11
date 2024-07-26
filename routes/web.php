0c4a34e718e47c7dc55d74cb2f6365a23b40ec81
Kirk John L. Sieras <bebekirk123@gmail.com>
2024-07-27 04:06:45 +0800

    Athentication for Admin middleware added

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -18,3 +10,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/adminRoute.php';
