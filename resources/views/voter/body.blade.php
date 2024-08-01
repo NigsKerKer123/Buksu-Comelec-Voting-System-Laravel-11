15193785dc1c031fef5511c97aa6946a59a05273
Kirk John L. Sieras <bebekirk123@gmail.com>
2024-08-01 21:00:48 +0800

    voter footer added

--- resources/views/voter/body.blade.php
+++ resources/views/voter/body.blade.php
@@ -55,5 +55,10 @@
                 </div>
             </div>
         </div>
+
+        <!-- footer action section -->
+        <div class="flex justify-end items-center h-auto mb-4 rounded bg-white dark:bg-gray-800 mr-4 ml-4 p-4">
+            <button type="button" class="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+        </div>
     </div>
 </div>
