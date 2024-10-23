b42c7c0a8afec5630ddfaa1faaf2723c238ac65f
Jaydee-31 <johndextergamo@gmail.com>
2024-10-24 02:51:08 +0800

    dev: login credentials value

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -45,12 +45,12 @@
                             <div class="text-base leading-6 space-y-5 text-gray-700 sm:text-lg sm:leading-7">
                                 @csrf
                                 <div class="relative lg:w-64">
-                                    <input autocomplete="off" id="email" name="email" type="email" class="rounded-lg peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="email" />
+                                    <input autocomplete="off" id="email" name="email" type="email" class="rounded-lg peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="email" value="gwapoko@gmail.com"/>
                                     <label for="email" class="px-3 absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-5 peer-focus:text-gray-600 peer-focus:text-sm">email</label>
                                 </div>
 
                                 <div class="relative">
-                                    <input autocomplete="off" id="password" name="password" type="password" class="rounded-lg peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                    <input autocomplete="off" id="password" name="password" type="password" class="rounded-lg peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value="gwapoko123"/>
                                     <label for="password" class="px-3 absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                                 </div>
 
